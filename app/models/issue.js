cc4d480cee9118991da243a5d57884c48ddf0c01
ke-pan <ke_pan@yeah.net>
2016-07-14 07:36:39 +0800

    feat: models

---
+++ app/models/issue.js
@@ -0,0 +1,12 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo, hasMany } from 'ember-data/relationships';
+
+export default Model.extend({
+  closedAt: attr('string'),
+  createdAt: attr('string'),
+  customer: belongsTo('customer'),
+  description: attr('string'),
+  employee: belongsTo('employee'),
+  status: attr('string')
+});
