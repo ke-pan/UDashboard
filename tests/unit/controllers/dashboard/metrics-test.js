e0b62fdf7f431caf7b56f342f7016c79faf8ba05
ke-pan <ke_pan@yeah.net>
2016-07-17 20:42:55 +0800

    feat: number of open issue

---
+++ tests/unit/controllers/dashboard/metrics-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('controller:dashboard/metrics', 'Unit | Controller | dashboard/metrics', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
