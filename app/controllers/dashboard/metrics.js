e6430883f40eb2d874b2d87a7b227d6703ad1744
ke-pan <ke_pan@yeah.net>
2016-07-18 15:53:46 +0800

    style: jshint

--- app/controllers/dashboard/metrics.js
+++ app/controllers/dashboard/metrics.js
@@ -1,9 +1,9 @@
 import Ember from 'ember';
 
-const { get, computed } = Ember
+const { get, computed } = Ember;
 
 export default Ember.Controller.extend({
   openingIssues: computed.filter('issues', function(issue) {
-    return get(issue, 'status') == 'opening';
+    return get(issue, 'status') === 'opening';
   })
 });
