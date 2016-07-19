62cc4acfb751b1f134d8b6953a6478f734390f59
ke-pan <ke_pan@yeah.net>
2016-07-19 11:16:14 +0800

    feat: seed the database

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -1,7 +1,7 @@
 export default function(server) {
-  for (var i = 0; i < 20; i++) {
+  for (var i = 0; i < 60; i++) {
     let employee = server.create('employee');
     let customer = server.create('customer');
-    server.createList('issue', 40, { employee, customer } );
+    server.createList('issue', 15, { employee, customer } );
   }
 }
