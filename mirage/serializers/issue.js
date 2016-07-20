bb24f5418639fc2b2dad33b819098becccee7edc
ke-pan <ke_pan@yeah.net>
2016-07-21 07:46:32 +0800

    fix: return all if no params

--- mirage/serializers/issue.js
+++ mirage/serializers/issue.js
@@ -5,8 +5,11 @@ export default BaseSerializer.extend({
   include: ['customer', 'employee'],
   serialize(object, request) {
     let json = BaseSerializer.prototype.serialize.apply(this, arguments);
-    return json.filter(function(issue) {
-      return moment(issue.createdAt).format("YYYY-MM") == request.queryParams.period
-    })
+    if (request.queryParams.period) {
+      return json.filter(function(issue) {
+        return moment(issue.createdAt).format("YYYY-MM") == request.queryParams.period
+      })
+    }
+    return json;
   }
 });
