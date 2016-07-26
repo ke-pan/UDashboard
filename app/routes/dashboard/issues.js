73e50ffa127c001cdd6ca972c952d388daa110e2
ke-pan <ke_pan@yeah.net>
2016-07-26 08:25:25 +0800

    feat: get json from local

--- app/routes/dashboard/issues.js
+++ app/routes/dashboard/issues.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  ajax: Ember.inject.service(),
+  model() {
+    const request = this.get('ajax').request('/data/issues.json', {
+      method: 'get'
+    });
+
+    return request.then((result) => JSON.parse(result));
+  }
 });
