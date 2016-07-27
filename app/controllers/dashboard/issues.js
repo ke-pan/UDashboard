97bd83b7202385b35e19f3c366219f1dfcc3d7a2
ke-pan <ke_pan@yeah.net>
2016-07-27 08:27:02 +0800

    feat: issue table

--- app/controllers/dashboard/issues.js
+++ app/controllers/dashboard/issues.js
@@ -1,4 +1,30 @@
 import Ember from 'ember';
+import moment from 'moment';
+import _ from 'lodash';
 
 export default Ember.Controller.extend({
+  queryParams: ['sort', 'dir'],
+  sort: 'submissionAt',
+  dir: 'desc',
+  issues: Ember.computed('model', 'dir', 'sort', function() {
+    let issues = this.get('model');
+    let dir = this.get('dir');
+    let sort = this.get('sort');
+    let sorted = _.sortBy(issues, sort);
+    if (dir === 'desc') sorted = sorted.reverse();
+    return (sorted
+      .map(function(item) {
+        let closedTime = item.closedAt ? moment(item.closedAt).format('YY-MM-DD, kk:mm') : '';
+        return {
+          submissionTime: moment(item.submissionAt).format('YY-MM-DD, kk:mm'),
+          closedTime,
+          customerName: item.customerName,
+          customerEmail: item.customerEmail,
+          description: item.description,
+          status: item.status,
+          employeeName: item.employeeName
+        };
+      })
+    );
+  })
 });
--- app/routes/dashboard/issues.js
+++ app/routes/dashboard/issues.js
@@ -3,10 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   ajax: Ember.inject.service(),
   model() {
-    const request = this.get('ajax').request('/data/issues.json', {
+    return this.get('ajax').request('/data/issues.json', {
       method: 'get'
     });
-
-    return request.then((result) => JSON.parse(result));
   }
 });
