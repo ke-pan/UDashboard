ccabb3f9a7ace5c648acc6bda55356a81bd1f681
ke-pan <ke_pan@yeah.net>
2016-07-31 23:03:47 +0800

    feat: rerender only change

--- app/routes/dashboard/locations.js
+++ app/routes/dashboard/locations.js
@@ -9,12 +9,27 @@ export default Ember.Route.extend({
   setupController(controller, model) {
     this._super(controller, model);
     this.set('refreshing', true);
-    Ember.run.later(this, this.refresh, 3000);
+    Ember.run.later(this, this.poll, 3000);
   },
-  refresh() {
-    this._super();
+  poll() {
+    let controller = this.controller;
     if (this.get('refreshing')) {
-      Ember.run.later(this, this.refresh, 3000);
+      Ember.$.get('data/locations.csv')
+      .then(this.get('csv2json').csv2json)
+      .then(results => {
+        let model = controller.get('model');
+        if (model.length !== results.length) {
+          controller.set('model', results);
+          return;
+        }
+        for (var i=0; i<results.length; i++) {
+          if (!Ember.isEqual(results[i], model[i])) {
+            controller.set('model', results);
+            return;
+          }
+        }
+      });
+      Ember.run.later(this, this.poll, 3000);
     }
   },
   actions: {
--- app/routes/dashboard/metrics.js
+++ app/routes/dashboard/metrics.js
@@ -12,12 +12,62 @@ export default Ember.Route.extend({
   setupController(controller, model) {
     this._super(controller, model);
     this.set('refreshing', true);
-    Ember.run.later(this, this.refresh, 3000);
+    Ember.run.later(this, this.pollIssues, 3000);
+    Ember.run.later(this, this.pollOpeningIssues, 3000);
+    Ember.run.later(this, this.pollCustomers, 3000);
   },
-  refresh() {
-    this._super();
+  pollIssues() {
+    let controller = this.controller;
     if (this.get('refreshing')) {
-      Ember.run.later(this, this.refresh, 3000);
+      Ember.$.get('data/monthly_issue_count.csv')
+      .then(this.get('csv2json').csv2json)
+      .then(results => {
+        let model = controller.get('issues');
+        if (model.length !== results.length) {
+          controller.set('issues', results);
+          return;
+        }
+        for (var i=0; i<results.length; i++) {
+          if (!Ember.isEqual(results[i], model[i])) {
+            controller.set('issues', results);
+            return;
+          }
+        }
+      });
+      Ember.run.later(this, this.pollIssues, 3000);
+    }
+  },
+  pollCustomers() {
+    let controller = this.controller;
+    if (this.get('refreshing')) {
+      Ember.$.get('data/monthly_customer_count.csv')
+      .then(this.get('csv2json').csv2json)
+      .then(results => {
+        let model = controller.get('customers');
+        if (model.length !== results.length) {
+          controller.set('customers', results);
+          return;
+        }
+        for (var i=0; i<results.length; i++) {
+          if (!Ember.isEqual(results[i], model[i])) {
+            controller.set('customers', results);
+            return;
+          }
+        }
+      });
+      Ember.run.later(this, this.pollCustomers, 3000);
+    }
+  },
+  pollOpeningIssues() {
+    let controller = this.controller;
+    if (this.get('refreshing')) {
+      Ember.$.getJSON('data/opening_issue.json')
+      .then(results => {
+        if (!Ember.isEqual(results, controller.get('openingIssues'))) {
+          controller.set('openingIssues', results);
+        }
+      });
+      Ember.run.later(this, this.pollOpeningIssues, 3000);
     }
   },
   actions: {
